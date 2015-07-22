c37f3cf558eb65bb61d518bc0bba68b6c66e564b
Sahap Asci <sahapasci@gmail.com>
2015-07-22 15:43:08 +0300

    updated for v 9.2

--- dba.sp_blocked_query.sql
+++ dba.sp_blocked_query.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION dba.sp_blocked_query (
+CREATE OR REPLACE FUNCTION sp_blocked_query (
   g_blocked_pid integer,
   out blocked_pid integer,
   out blocked_user varchar,
@@ -23,21 +23,21 @@ RETURNS SETOF record AS
 $body$
 BEGIN
   SELECT
-      psa_bed.procpid AS blocked_pid,
+      psa_bed.pid AS blocked_pid,
       psa_bed.usename::VARCHAR AS blocked_user,
       age(now(), psa_bed.backend_start) AS blocked_process_age,
       age(now(), psa_bed.xact_start) AS blocked_transaction_age,
       age(now(), psa_bed.query_start) AS blocked_query_age,
-      psa_bed.current_query::VARCHAR AS blocked_query,
+      psa_bed.query::VARCHAR AS blocked_query,
       psa_bed.application_name::VARCHAR AS blocked_application_name,
       psa_bed.client_addr::VARCHAR AS blocked_client_addr,
       psa_bed.client_port::VARCHAR AS blocked_client_port,
-      psa_bing.procpid AS blocking_pid,
+      psa_bing.pid AS blocking_pid,
       psa_bing.usename::VARCHAR AS blocking_user,
       age(now(), psa_bing.backend_start) AS blocking_process_age,
       age(now(), psa_bing.xact_start) AS blocking_transaction_age,
       age(now(), psa_bing.query_start) AS blocking_query_age,
-      psa_bing.current_query::VARCHAR AS blocking_query,
+      psa_bing.query::VARCHAR AS blocking_query,
       psa_bing.application_name::VARCHAR AS blocking_application_name,
       psa_bing.client_addr::VARCHAR AS blocking_client_addr,
       psa_bing.client_port::VARCHAR AS blocking_client_port
@@ -61,7 +61,7 @@ BEGIN
       blocking_client_addr,
       blocking_client_port
     FROM pg_catalog.pg_locks l_bed
-    JOIN pg_catalog.pg_stat_activity psa_bed ON l_bed.pid = psa_bed.procpid
+    JOIN pg_catalog.pg_stat_activity psa_bed ON l_bed.pid = psa_bed.pid
     JOIN pg_catalog.pg_locks l_bing ON l_bed.locktype = l_bing.locktype AND
                                        l_bed.database IS NOT DISTINCT FROM l_bing.database AND 
                                        l_bed.relation IS NOT DISTINCT FROM l_bing.relation AND 
@@ -73,7 +73,7 @@ BEGIN
                                        l_bed.objid IS NOT DISTINCT FROM l_bing.objid AND 
                                        l_bed.objsubid IS NOT DISTINCT FROM l_bing.objsubid AND 
                                        l_bed.pid <> l_bing.pid
-    JOIN pg_catalog.pg_stat_activity psa_bing ON l_bing.pid = psa_bing.procpid
+    JOIN pg_catalog.pg_stat_activity psa_bing ON l_bing.pid = psa_bing.pid
     WHERE 
       l_bed.pid = g_blocked_pid AND
       l_bing.granted IS TRUE AND
@@ -81,7 +81,7 @@ BEGIN
 
     IF FOUND THEN
       RETURN NEXT;
-      RETURN QUERY SELECT * FROM dba.sp_blocked_query(blocking_pid);
+      RETURN QUERY SELECT * FROM sp_blocked_query(blocking_pid);
     END IF;
     
 END;
