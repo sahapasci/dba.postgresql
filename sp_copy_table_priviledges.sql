f1385846c4e9f52ed24cbea52d69bbb9ab8c725e
Sahap Asci <sahapasci@gmail.com>
2015-11-16 09:25:17 +0200

    the function "dba.sp_copy_table_priviledges" is now works correctly. execute is now and option. Moreover executes sql's are now returned as text output.

--- sp_copy_table_priviledges.sql
+++ sp_copy_table_priviledges.sql
@@ -1,17 +1,19 @@
 CREATE OR REPLACE FUNCTION dba.sp_copy_table_priviledges (
-  i_from_schema varchar,
-  i_from_table varchar,
-  i_to_schema varchar,
-  i_to_table varchar,
-  i_revoke_previous boolean = true
+  i_from_schema text,
+  i_from_table text,
+  i_to_schema text,
+  i_to_table text,
+  i_revoke_previous boolean,
+  i_execute boolean = false,
+  out sql text
 )
-RETURNS void AS
+RETURNS SETOF text AS
 $body$
 DECLARE
   l_sql VARCHAR;
 BEGIN
   IF i_revoke_previous THEN
-    FOR l_sql IN 
+    FOR sql IN 
       SELECT 
           'REVOKE ' || string_agg(tg.privilege_type, ', ') || ' ON ' || tg.table_schema || '.'|| tg.table_name || ' FROM ' || tg.grantee || ';' 
         FROM information_schema.role_table_grants tg
@@ -20,35 +22,45 @@ BEGIN
           tg.table_name = i_to_table
         GROUP BY tg.grantee, tg.table_schema, tg.table_name
       LOOP
-        EXECUTE l_sql;
+        IF i_execute THEN
+          EXECUTE sql;
+        END IF;
+        RETURN NEXT;
       END LOOP;  
   END IF;
 
-  FOR l_sql IN 
+  FOR sql IN 
     SELECT 
-        'GRANT ' || string_agg(tg.privilege_type, ', ') || ' ON ' || tg.table_schema || '.'|| tg.table_name || ' TO ' || tg.grantee || ';' 
+        'GRANT ' || string_agg(tg.privilege_type, ', ') || ' ON ' || i_to_schema || '.'|| i_to_table || ' TO ' || tg.grantee || ';' 
       FROM information_schema.role_table_grants tg
       WHERE 
         tg.table_schema = i_from_schema AND
         tg.table_name = i_from_table
       GROUP BY tg.grantee, tg.table_schema, tg.table_name
     LOOP
-      EXECUTE l_sql;
+      IF i_execute THEN
+        EXECUTE sql;
+      END IF;    
+      RETURN NEXT;
     END LOOP;
     
   SELECT 
-      'ALTER TABLE ' || i_from_schema || '.' || i_from_table || ' OWNER TO '|| t.tableowner || ';'
+      'ALTER TABLE ' || i_to_schema || '.' || i_to_table || ' OWNER TO '|| t.tableowner || ';'
     INTO
-      l_sql
+      sql
     FROM pg_tables t
     WHERE t.schemaname = i_from_schema AND
           t.tablename = i_from_table;    
-  EXECUTE l_sql;
+  
+  IF i_execute THEN
+    EXECUTE sql;
+  END IF;
+  RETURN NEXT;
     
 END;
 $body$
 LANGUAGE 'plpgsql'
 VOLATILE
-CALLED ON NULL INPUT
+RETURNS NULL ON NULL INPUT
 SECURITY INVOKER
-COST 100;
+COST 100 ROWS 1000;
