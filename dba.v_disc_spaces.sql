111d3cde4ebbfc9400b21ef72b880fbe4e8cb38b
Sahap Asci <sahapasci@gmail.com>
2015-11-23 11:56:42 +0200

    v_disc_spaces now displays database's default tablespace instead of null

--- dba.v_disc_spaces.sql
+++ dba.v_disc_spaces.sql
@@ -1,12 +1,19 @@
 CREATE OR REPLACE VIEW dba.v_disc_spaces AS
+WITH tbl_spc AS (
+  SELECT ts.spcname AS spcname
+    FROM pg_tablespace ts 
+    JOIN pg_database db ON db.dattablespace = ts.oid
+    WHERE db.datname = current_database()
+)
+(
 SELECT
     t.schemaname,
     t.tablename,
     pg_table_size('"' || t.schemaname || '"."' || t.tablename || '"') AS table_disc_size,
     NULL as index,
     0 as index_disc_size,
-    t.tablespace
-  FROM pg_tables t
+    COALESCE(t.tablespace, ts.spcname) AS tablespace
+  FROM pg_tables t, tbl_spc ts
 
 UNION ALL 
 
@@ -16,5 +23,6 @@ SELECT
     0,
     i.indexname,
     pg_relation_size('"' || i.schemaname || '"."' || i.indexname || '"'),
-    i.tablespace
-  FROM pg_indexes i;
+    COALESCE(i.tablespace, ts.spcname)
+  FROM pg_indexes i, tbl_spc ts
+) 
